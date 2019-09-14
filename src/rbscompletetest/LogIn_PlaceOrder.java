e63feba6e944e00ff3e2a2b054ebbc0e7ca03e38
Shona_PC <Shona_PC@192.168.1.188>
2019-09-14 08:04:07 +0100

    Final Codeline on 14-Sept-2019

--- src/rbscompletetest/LogIn_PlaceOrder.java
+++ src/rbscompletetest/LogIn_PlaceOrder.java
@@ -4,93 +4,176 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+//import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
-//import org.openqa.selenium.*;
+import rbscompletetest.signup;
 
 public class LogIn_PlaceOrder {
 
 		public static void main(String[] args) {
-			 
-			System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");		 
+			
+			// Point the web driver to local Chrome driver
+			System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");	
+			
 			// Initialize browser
-			WebDriver driver=new ChromeDriver();		 
+			WebDriver driver=new ChromeDriver();
+			
 			// Maximize browser		 
 			driver.manage().window().maximize();
+			//driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+			
 			// Launch the application
 			String baseUrl ="http://automationpractice.com";
-			//improvise sign on click then uncomment below code
-			//String baseUrl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";     
+			
+			//Direct sign in page then uncomment below code and comment above
+			//String baseUrl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";  
+			
 			driver.get(baseUrl);
+			
+			//Create Account
+			String[] userName_PWD = signup.Signup(driver);
+			String userName = userName_PWD[0];
+			String PWD = userName_PWD[1];
+			
 			//Click on Sign in 
 			driver.findElement(By.xpath("//header[@id='header']/div[2]/div/div/nav/div/a")).click();
+			
 			//Key in Credentials
-			driver.findElement(By.xpath("//div[2]/form/div/div/input")).sendKeys("Someonetest@exampletest.com");
-			driver.findElement(By.xpath("//span/input")).sendKeys("Password123");
-			//Click log in 
+			driver.findElement(By.xpath("//div[2]/form/div/div/input")).sendKeys(userName);
+			driver.findElement(By.xpath("//span/input")).sendKeys(PWD);
+			
+			//and Click log in 
 			driver.findElement(By.xpath("//p[2]/button/span")).click();
 			
-			//Update Personal Information
-			//Click on Personal Information
+			//**********************Update Personal Information STARTs*******************************
+			
+			//Click on Personal Information link
 			driver.findElement(By.xpath("//li[4]/a/span")).click();
+			
 			//update first name replacing the current one
+			String oldName = driver.findElement(By.xpath("//div[2]/input")).getAttribute("value");
 			driver.findElement(By.xpath("//div[2]/input")).clear();
-			driver.findElement(By.xpath("//div[2]/input")).sendKeys("SatyashreeUpdate");
+			driver.findElement(By.xpath("//div[2]/input")).sendKeys("MickeyandMinnie");
+			String newName = driver.findElement(By.xpath("//div[2]/input")).getAttribute("value");
+			
 			//change password
-			driver.findElement(By.xpath("//div[6]/input")).sendKeys("Password123");
-			driver.findElement(By.xpath("//div[7]/input")).sendKeys("Password123");
-			driver.findElement(By.xpath("//input[@id='confirmation']")).sendKeys("Password123");
+			driver.findElement(By.xpath("//div[6]/input")).sendKeys(PWD);
+			driver.findElement(By.xpath("//div[7]/input")).sendKeys(PWD);
+			driver.findElement(By.xpath("//input[@id='confirmation']")).sendKeys(PWD);
+			
 			//click submit
 			driver.findElement(By.xpath("//div[@id='center_column']/div/form/fieldset/div[11]/button/span")).click();
-			//order tshirt
+			
+			// Error after save
+			Boolean isErrorSaveName = driver.findElements(By.xpath("//p[contains(.,' Your personal information has been successfully updated.')]")).size()>0;
+			if(isErrorSaveName) {
+				Boolean isErrorSaveNameDisplayed = driver.findElement(By.xpath("//p[contains(.,' Your personal information has been successfully updated.')]")).isDisplayed();
+				if (isErrorSaveNameDisplayed) {
+					System.out.println("First Name has been updated from '"+ oldName +"' to '"+ newName + "'");
+				};
+			}
+			else {
+				System.out.println("There was some error while updating the first name. Please try again");
+			};
+			
+			
+			//**********************Update Personal Information ENDs*******************************
+			
+			
+			//*********************Place an Order for T-shirt STARTs******************************************
+			
 			//Click for tshirt tab
 			driver.findElement(By.xpath("//div[@id='block_top_menu']/ul/li[3]/a")).click();
+			
 			//Select the specific Tshirt
-			driver.findElement(By.cssSelector("a[title=\"Faded Short Sleeve T-shirts\"]")).click();
+			driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li/div/div[1]/div/a[1]/img")).click();
+						
 			//Select the Quick View Frame
 			driver.switchTo().frame(0);
+			
 			// Select Quantity
 			driver.findElement(By.xpath("//div[2]/p/input")).clear();
 			driver.findElement(By.xpath("//div[2]/p/input")).sendKeys("2");
+			
 			//Select the Size
 			driver.findElement(By.id("group_1")).sendKeys("S");
+			
 			//Select the color
 			driver.findElement(By.xpath("//fieldset[2]/div/ul/li[2]/a")).click();
+			
 			//Add to Cart
 			driver.findElement(By.xpath("//div[3]/div/p/button")).click();
-			//click on proceed to checkout in the pop up
-			WebDriverWait wait=new WebDriverWait(driver, 20);
+			
+			//click on proceed to checkout on the pop up menu
+			WebDriverWait wait=new WebDriverWait(driver, 30);
 			WebElement ProceedtoCheckoutlink;
-			ProceedtoCheckoutlink= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath( "//span[contains(.,'Proceed to checkout')]")));
+			ProceedtoCheckoutlink= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath( "//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a")));
 			ProceedtoCheckoutlink.click();
-			//Click on Proceed to Checkout in Summary
+			
+			//Click on Proceed to Checkout in Summary tab
 			driver.switchTo().defaultContent();
 			driver.findElement(By.xpath("//div[@id='center_column']/p[2]/a/span")).click();
-			//Click on Proceed to Checkout in Address
+			
+			//Click on Proceed to Checkout in Address tab
 			driver.findElement(By.xpath("//div[@id='center_column']/form/p/button/span")).click();
-			//Click on Proceed to Checkout in Shipping
+			
+			//Click on Proceed to Checkout in Shipping tab
 			// Confirm Terms and Service
 			driver.findElement(By.xpath("//input[@id='cgv']")).click();
-			//Then Proceed to Checkout
+			
+			//Final Proceed to Checkout
 			driver.findElement(By.xpath("//form[@id='form']/p/button/span")).click();
+			
 			//click on Pay by bankwire
 			driver.findElement(By.xpath("//div[@id='HOOK_PAYMENT']/div/div/p/a")).click();
-			//click on confirm my order
+			
+			//Confirm order
 			driver.findElement(By.xpath("//p/button/span")).click();
-			//Get the Order Id
+			
+			//Get the Order Id from the invoice details
 			String OrderId = driver.findElement(By.xpath("//div[@id='center_column']/div")).getText().substring(216,225);
-			//Go to Order History & get the Order Id
+			
+			//Go to Order History
 			driver.findElement(By.xpath("//div[@id='center_column']/p/a")).click();
-			String HisOrderId = driver.findElement(By.xpath("//table[@id='order-list']/tbody/tr/td")).getText();
-			//Verify Order in Order history
-			System.out.println(OrderId);
-			System.out.println(HisOrderId);
-			if (OrderId.equals(HisOrderId)){
-				System.out.println("Order has been placed Successfully.");
+			
+			//Find the number orders in Order History
+			int rowNum = driver.findElements(By.xpath("//*[@id=\"order-list\"]/tbody/tr")).size();
+			
+			//Order Id is available on 1st Column
+			int colNum = 1;
+			
+			//Set a flag for use
+			boolean orderPlaced = false;
+			
+			//Loop through Order history
+			WebElement BooksTable = driver.findElement(By.xpath("//*[@id=\"order-list\"]"));
+		    List<WebElement> rowVals = BooksTable.findElements(By.tagName("tr"));
+			for(int i=1; i<=rowNum; i++){
+		        List<WebElement> colVals = rowVals.get(i).findElements(By.tagName("td"));
+				for(int j=0; j<=colNum; j++){
+		        		String HisOrderId = colVals.get(j).getText();
+		        		
+		        		//Match the Order id and confirm if Order has been placed successfully
+		        		if (OrderId.equals(HisOrderId)){
+		    				System.out.println("Order has been placed successfully with OrderId '"+HisOrderId+"'");
+		    				orderPlaced = true;
+		    			}
+		        	}
+		        }
+			
+			// Order id did not match in entire Order History
+			if (!orderPlaced) {
+				System.out.println("Order is not available in Order history. Please try again placing the order");
 			}
-			else {
-				System.out.println("Order is not available in Order history.");
-			}
+			
+			//Close the Browser
 			driver.close();
+			
+			// Close down the system variable
 		    System.exit(0);
 		    
 		}
--- src/rbscompletetest/signup.java
+++ src/rbscompletetest/signup.java
@@ -1,55 +1,120 @@
 package rbscompletetest;
 
-import java.util.concurrent.TimeUnit;
+//import java.util.List;
+import java.util.Random;
+//import java.util.UUID;
+//import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class signup {
 
-	public static void main(String[] args) {
-	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");		 
-	// Initialize browser
-	WebDriver driver=new ChromeDriver();		 
-	// Maximize browser		 
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-	// Launch the application
-	String baseUrl ="http://automationpractice.com";
-	driver.get(baseUrl);
+	public static String[] Signup(WebDriver driver) {
+		
+	String[] eMail_PWD = new String[2];
+
+	//Click on Login
 	driver.findElement(By.xpath("//header[@id='header']/div[2]/div/div/nav/div/a")).click();
+	
+	//Clear the existing Add account email value if any
 	driver.findElement(By.xpath("//input[@id='email_create']")).clear();
-	driver.findElement(By.xpath("//input[@id='email_create']")).sendKeys("Someonetest5@exampletest.com");
+	
+	//Generate random email id and submit for account creation
+	String randomEmail = randomEmail();
+	driver.findElement(By.xpath("//input[@id='email_create']")).sendKeys(randomEmail);
 	driver.findElement(By.xpath("//button[@id='SubmitCreate']/span")).click();
-	WebDriverWait wait=new WebDriverWait(driver, 20);
-	WebElement SelectGenderlink;
-	SelectGenderlink= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath( "//input[@id='id_gender2']")));
-	SelectGenderlink.click();
+	
+	// If the email Id is already present then system throws an error, in that case create another random email Id
+	Boolean isAccountErrorPresent = driver.findElements(By.id("create_account_error")).size()>0;
+	
+	//Repeat until email id is accepted
+	 while(isAccountErrorPresent) {
+		WebDriverWait wait=new WebDriverWait(driver, 60);
+		Boolean isAccountErrorDisplayed = driver.findElement(By.id("create_account_error")).isDisplayed();
+		
+		// If Email Id not accepted then go for next email id
+		if(isAccountErrorDisplayed) {
+			System.out.println(randomEmail +" was already there hence trying new id");
+			randomEmail = randomEmail();
+			driver.findElement(By.xpath("//input[@id='email_create']")).clear();
+			driver.findElement(By.xpath("//input[@id='email_create']")).sendKeys(randomEmail);
+		}else {
+			driver.findElement(By.xpath("//button[@id='SubmitCreate']/span")).click();
+			
+			//if Email Id is accepted the first field of next page is Gender
+			WebElement GenderLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath( "//*[@id=\"uniform-id_gender1\"]")));
+			GenderLink.click();
+		};
+		isAccountErrorPresent = driver.findElements(By.id("create_account_error")).size()>0;
+	};
+	
+	// Name 
 	driver.findElement(By.xpath("//input[@id='customer_firstname']")).clear();
 	driver.findElement(By.xpath("//input[@id='customer_firstname']")).sendKeys("Minnie");
 	driver.findElement(By.xpath("//input[@id='customer_lastname']")).clear();
 	driver.findElement(By.xpath("//input[@id='customer_lastname']")).sendKeys("Mouse");
+	
+	// Password
+	String randomPWD = randomPWD();
 	driver.findElement(By.xpath("//input[@id='passwd']")).clear();
-	driver.findElement(By.xpath("//input[@id='passwd']")).sendKeys("Password123");
+	driver.findElement(By.xpath("//input[@id='passwd']")).sendKeys(randomPWD);
+	
+	// D.O.B
 	driver.findElement(By.id("days")).sendKeys("1");
 	driver.findElement(By.id("months")).sendKeys("January");
 	driver.findElement(By.id("years")).sendKeys("1989");
+	
+	//Address
 	driver.findElement(By.id("address1")).sendKeys("Address1");
 	driver.findElement(By.id("address2")).sendKeys("Address2");
 	driver.findElement(By.id("city")).sendKeys("City1");
 	driver.findElement(By.id("id_state")).sendKeys("California");
 	driver.findElement(By.xpath("//input[@id='postcode']")).clear();
 	driver.findElement(By.xpath("//input[@id='postcode']")).sendKeys("00123");
+	
+	//Contacts
 	driver.findElement(By.xpath("//input[@id='phone']")).clear();
 	driver.findElement(By.xpath("//input[@id='phone']")).sendKeys("0123456789");
 	driver.findElement(By.xpath("//input[@id='phone_mobile']")).clear();
 	driver.findElement(By.xpath("//input[@id='phone_mobile']")).sendKeys("0123456789");
+	
+	//Submit for Account Creation
 	driver.findElement(By.xpath("//div[4]/button/span")).click();
-	driver.findElement(By.xpath("//a[@href='http://automationpractice.com/index.php?mylogout=']")).click();
-	driver.close();
-    System.exit(0);
+	
+	//There is no successful message after submit for signup. But on Successful account creation system returns to HomePage
+	WebDriverWait wait=new WebDriverWait(driver, 60);
+	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath( "//p[contains(.,'Welcome to your account. Here you can manage all of your personal information and orders.')]")));
+	Boolean isAccountCreated = driver.findElements(By.xpath("//p[contains(.,'Welcome to your account. Here you can manage all of your personal information and orders.')]")).size()>0;
+	
+	//So if HomePage appears then Account has been created
+	if(isAccountCreated) {
+	System.out.println("Account has been created for '"+randomEmail+"' with password as '"+randomPWD+"'");
+	}
+	else {
+		System.out.println("Some Issue with Account creation for emailid '"+randomEmail+"'. Hence closing the browser. Please try again");
+		driver.close();
+	};
+	
+	//Logout
+	driver.findElement(By.xpath("//a[@href='http://automationpractice.com/index.php?mylogout=']")).click();	
+	
+	//Return EmailId
+	eMail_PWD[0]= randomEmail; 
+	eMail_PWD[1]= randomPWD; 
+	return eMail_PWD;
 }
+	private static String randomEmail() {
+		Random randomGenerator = new Random();  
+		int randomInt = randomGenerator.nextInt(); 
+		return "Someone"+ randomInt +"@example.com";
+    }
+	private static String randomPWD() {
+		Random randomGenerator = new Random();  
+		int randomInt = randomGenerator.nextInt(1000); 
+		return "Password"+ randomInt;
+    }
 	}
